11c74e6fee11a536fb0034fd3c8e034d6c56def3
aifusenno1 <aifusenno1@gmail.com>
2017-10-03 17:49:14 -0500

    comment out some code in syscall to make the files able to compile

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -3,8 +3,22 @@
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "threads/vaddr.h"
 
 static void syscall_handler (struct intr_frame *);
+static void halt (void);
+static void exit (int status);
+static pid_t exec (const char *file);
+static int wait (pid_t);
+static bool create (const char *file, unsigned initial_size);
+static bool remove (const char *file);
+static int open (const char *file);
+static int filesize (int fd);
+static int read (int fd, void *buffer, unsigned length);
+static int write (int fd, const void *buffer, unsigned length);
+static void seek (int fd, unsigned position);
+static unsigned tell (int fd);
+static void close (int fd);
 
 void
 syscall_init (void)
@@ -16,45 +30,64 @@ static void
 syscall_handler (struct intr_frame *f UNUSED)
 {
    void * sp = f->esp; // a copy of stack pointer; do not want to modify the stack pointer
-   // check if the pointer is valid; but wondering if this is necessary, because does the user set the pointer themselves?
+   // this is not the best way to check memory access, but will do this for now.
+   // check if the pointer is valid; but wondering if this is necessary, because does the user set the pointer themselves for syscalls?
    // check if it's a user space virtual address
    // check if the virtual memory is mapped
    // check if it's a null pointer
-   if (sp == NULL || is_kernel_vaddr(sp))
+   if (sp == NULL || is_kernel_vaddr(sp))     // how to check if it's mapped?
+      exit(1);  // don't how exactly we should terminate the process; will do this for now
+   // But do we need to check the validity each time we increment the pointer?
 
-   int syscall_num = *sp;
-   ((uint32_t *)sp)++;
+   int syscall_num = *(int *)sp;  // my understanding is, this assignment triggers syscall read automatically. And when run in Pintos, the syscall we implemented is used, not the one from C library
+   sp = (uint32_t *)sp + 1;
+
    switch (syscall_num) {
-      case SYS_HALT:
+      case SYS_HALT: {
          halt();
       break;
-      case SYS_EXIT:
-         int status = *sp;
-         exit();
-      break;
-      case SYS_EXEC:
-      break;
-      case SYS_WAIT:
-      break;
-      case SYS_CREATE:
-      break;
-      case SYS_REMOVE:
-      break;
-      case SYS_OPEN:
-      break;
-      case SYS_FILESIZE:
-      break;
-      case SYS_READ:
+      }
 
+      case SYS_EXIT: {
+         int status = *(int *)sp;
+         exit(status);
       break;
-      case SYS_WRITE:
-      break;
-      case SYS_SEEK:
-      break;
-      case SYS_TELL:
-      break;
-      case SYS_CLOSE:
-      break;
+      }
+
+      case SYS_EXEC: {
+
+      }
+      case SYS_WAIT: {
+
+      }
+      case SYS_CREATE: {
+
+      }
+      case SYS_REMOVE: {
+
+      }
+      case SYS_OPEN: {
+
+      }
+      case SYS_FILESIZE: {
+
+      }
+      case SYS_READ: {
+
+      }
+
+      case SYS_WRITE: {
+
+      }
+      case SYS_SEEK: {
+
+      }
+      case SYS_TELL: {
+
+      }
+      case SYS_CLOSE: {
+         
+      }
 
    }
 
@@ -72,47 +105,47 @@ void exit (int status) {
 
 }
 
-pid_t exec (const char *file) {
-
-}
-
-int wait (pid_t) {
-
-}
-
-bool create (const char *file, unsigned initial_size) {
-
-}
-
-bool remove (const char *file) {
-
-}
-
-int open (const char *file) {
-
-}
-
-int filesize (int fd) {
-
-}
-
-int read (int fd, void *buffer, unsigned length) {
-
-}
-
-int write (int fd, const void *buffer, unsigned length) {
-
-}
-
-void seek (int fd, unsigned position) {
-
-}
-
-unsigned tell (int fd) {
-
-}
-
-void close (int fd) {
-
-}
+// pid_t exec (const char *file) {
+//
+// }
+//
+// int wait (pid_t) {
+//
+// }
+//
+// bool create (const char *file, unsigned initial_size) {
+//
+// }
+//
+// bool remove (const char *file) {
+//
+// }
+//
+// int open (const char *file) {
+//
+// }
+//
+// int filesize (int fd) {
+//
+// }
+//
+// int read (int fd, void *buffer, unsigned length) {
+//
+// }
+//
+// int write (int fd, const void *buffer, unsigned length) {
+//
+// }
+//
+// void seek (int fd, unsigned position) {
+//
+// }
+//
+// unsigned tell (int fd) {
+//
+// }
+//
+// void close (int fd) {
+//
+// }
 
